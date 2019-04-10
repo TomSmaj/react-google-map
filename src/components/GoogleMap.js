015b1ab708f894490d73b4507decab70a24c6764
Thomas Smajstrla <smajstrla@google.com>
2019-04-10 17:02:04 -0500

    attempting to add 3 markers, currently not appearing on map

--- src/components/GoogleMap.js
+++ src/components/GoogleMap.js
@@ -6,10 +6,12 @@ class GoogleMap extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            map: {}
+            map: {},
+            markers: []
         };
         this.mapRef = React.createRef();
         this.createMap = this.createMap.bind(this);
+        // this.createMarkers = this.createMarkers.bind(this);
     }
 
     componentDidMount(){
@@ -17,13 +19,35 @@ class GoogleMap extends Component {
     }
 
     createMap = () => {
-        var tempMap = new window.google.maps.Map(this.mapRef.current);
-        tempMap.setCenter({ lat: 30.2672, lng: -97.7431 });
-        tempMap.setZoom(13);
         this.setState({
-            map: tempMap
+            map: new window.google.maps.Map(this.mapRef.current)
         })
+        this.state.map.setCenter({ lat: 30.2672, lng: -97.7431 });
+        this.state.map.setZoom(13);
+        
+        var waterloo = {lat: 30.271849, lng: -97.754284};
+        var antones = {lat: 30.296238, lng: -97.742591};
+        var afs = {lat: 30.324586, lng: -97.708802};
+        var arr = [waterloo, antones, afs];
+
+        var tempArr;
+        for(let i = 0; i++; i< arr.length){
+            tempArr = this.state.markers;
+            tempArr.push(new window.google.maps.Marker({position: arr[i], map: this.state.map}))
+            this.setState({markers: tempArr});
+        }
     }
+
+    // createMarkers = () => {
+    //     var waterloo = {lat: 30.271849, lng: -97.754284};
+    //     var antones = {lat: 30.296238, lng: -97.742591};
+    //     var afs = {lat: 30.324586, lng: -97.708802};
+    //     var arr = [waterloo, antones, afs];
+
+    //     for(let i = 0; i++; i< arr.length){
+    //         var marker = new window.google.maps.Marker({position: arr[i], map: this.state.map});
+    //     }
+    // }
 
     render() {
         return (
